df0d19d96642554d54cd9f4497990fb81792fd0b
Muddasir Nazir <muddasirnazir92@gmail.com>
2023-01-31 00:04:42 +0500

    changes

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Slider from "react-slick";
 
-// import "react-double-range-slider/dist/cjs/index.css";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -27,14 +26,7 @@ import {
   DropDownIcon,
   ArrowDownIcon,
 } from "../Icons";
-import {
-  CopyIcon,
-  RoundCrossIcon,
-  SearchIcon,
-  HorzontalMenuIcon,
-  HeartIcon,
-  HeartFillIcon,
-} from "../Icons";
+import { CopyIcon, RoundCrossIcon, SearchIcon, HorzontalMenuIcon, HeartIcon, HeartFillIcon } from "../Icons";
 
 import HotBids from "../components/HotBids";
 import LiveAuctions from "../components/LiveAuctions";
@@ -54,21 +46,6 @@ const Main = ({ mintHandler }) => {
     let categories = [];
     // res data doe'nst have images , so adding data front-endly
     setNftTabs(res?.data);
-    // setNftTabs([
-    //   { lbl: "All NFTs", ico: "./images/NFT All 1.svg", id : 0 },
-    //   { lbl: "Art", ico: "./images/NFT Art 1.svg", id : 1 },
-    //   { lbl: "Music", ico: "./images/NFT Music 1.svg", id : 2 },
-    //   { lbl: "Motion", ico: "./images/NFT Motion 1.svg", id : 3 },
-    //   { lbl: "Sports", ico: "./images/NFT Sports 1.svg", id : 4 },
-    //   { lbl: "Trading Cards", ico: "./images/NFT Cards 1.svg", id : 5 },
-    //   { lbl: "Collectibles", ico: "./images/NFT Collectibles 1.svg", id : 6 },
-    //   { lbl: "Others", ico: "./images/NFT Others 1.svg", id : 7 },
-    // ])
-    // res.data.map((item) => {
-    //   if(item.category_name == "Art"){
-    //     categories.push({ lbl: "Art", ico: "./images/NFT Art 1.svg" })
-    //   }
-    // })
   };
   const [activecategory, setactivecategory] = useState("");
   const [categoryitems, setcategoryitems] = useState([]);
@@ -77,7 +54,7 @@ const Main = ({ mintHandler }) => {
   const [searchitems, setsearchitems] = useState("");
   const [showsearchitems, setshowsearchitems] = useState(false);
   const [searchloader, setsearchloader] = useState(false);
-  console.log("searchitems",searchitems)
+  console.log("searchitems", searchitems);
   const searchbackend = async () => {
     try {
       const res = await axios.post(`${process.env.REACT_APP_API_URL}/sale/search`, {
@@ -86,7 +63,7 @@ const Main = ({ mintHandler }) => {
       });
       console.log("searchbackend", res);
       if (res?.data) {
-        setsearchitems(res?.data?.rows)
+        setsearchitems(res?.data?.rows);
         //setsearchitems(res?.data[0]);
         setsearchloader(false);
       }
@@ -112,7 +89,7 @@ const Main = ({ mintHandler }) => {
   const api_getItemsByCategories = async () => {
     try {
       const res = await axios.get(
-        `${process.env.REACT_APP_API_URL}/category/getItemsByCategories/?id=${activecategory.id}&page_number=${pagenumber}&page_size=${pagesize}`
+        `${process.env.REACT_APP_API_URL}/category/getItemsByCategories/?id=${activecategory.id}&page_number=${pagenumber}&page_size=${pagesize}`,
       );
       console.log("getItemsByCategories_res", res);
       if (res?.data) {
@@ -152,14 +129,7 @@ const Main = ({ mintHandler }) => {
   ]);
   return (
     <div className="home-p flex flex-col">
-      {tab == "home" ? (
-        <div
-          className="home-sec flex jc"
-          // style={{ backgroundImage: `url(./images/bg-home.png)` }}
-        ></div>
-      ) : (
-        <></>
-      )}
+      {tab == "home" ? <div className="home-sec flex jc"></div> : <></>}
       <div className="container flex">
         <div className="wrapWidth wrap flex flex-col">
           <Filters setsearch={setsearch} />
@@ -174,8 +144,7 @@ const Main = ({ mintHandler }) => {
                     setactivecategory(item);
                   }}
                   key={i}
-                  className="tab-item flex aic"
-                >
+                  className="tab-item flex aic">
                   <img src={item.icon} className="icon" />
                   <div className="tag">{item.category_name}</div>
                 </div>
@@ -222,26 +191,18 @@ const Main = ({ mintHandler }) => {
       {tab === "home" && (
         <div className="our-app flex flex-col">
           <div className="wrapWidth wrap flex flex-col">
-            <div className="pg-hdr">
-              Download the Digital Block Exchange app to explore NFTs
-            </div>
+            <div className="pg-hdr">Download the Digital Block Exchange app to explore NFTs</div>
             <div className="meta flex aic jc">
               <div className="left flex aic jc">
                 <img src="./images/pc-shap.png" className="img" />
               </div>
               <div className="right flex">
                 <div className="meta flex  flex-col">
-                  <div className="desc">
-                    Digital Block Exchange website & app provides you with
-                    simple & fast trading options!
-                  </div>
+                  <div className="desc">Digital Block Exchange website & app provides you with simple & fast trading options!</div>
                   <div className="apps-link flex aic jc flex-col">
                     <div className="btn button flex aic">
                       <div className="logo flex aic jc">
-                        <img
-                          src="./images/apple_logo.svg"
-                          className="logo-img"
-                        />
+                        <img src="./images/apple_logo.svg" className="logo-img" />
                       </div>
                       <div className="txt flex flex-col">
                         <div className="lbl">Download on the</div>
@@ -250,10 +211,7 @@ const Main = ({ mintHandler }) => {
                     </div>
                     <div className="btn button flex aic">
                       <div className="logo flex aic jc">
-                        <img
-                          src="./images/google_store_logo.svg"
-                          className="logo-img"
-                        />
+                        <img src="./images/google_store_logo.svg" className="logo-img" />
                       </div>
                       <div className="txt flex flex-col">
                         <div className="lbl">GET IT ON</div>
