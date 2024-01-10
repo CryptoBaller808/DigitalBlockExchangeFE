f8b24444ce3783483da940c3509a02217214b533
gurpreet991 <gurpreet.singh@ellocentlabs.com>
2024-01-10 12:16:49 +0530

    feat(wallet): re sync wallet on refresh

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Web3 from "web3";
 import axios from "axios";
@@ -62,6 +62,8 @@ function App() {
   const [openSidebar, setOpenSidebar] = useState(false);
   const dispatch = useDispatch();
   const { isDarkMode } = useSelector(state => state.themeReducer);
+  const balanceData = useSelector(state => state.signInData?.balance);
+  const userToken = useMemo(() => balanceData?.userToken ?? "", [balanceData]);
 
   const socket = useSocket();
 
@@ -93,11 +95,16 @@ function App() {
     gettokenlocalstorage();
   }, []);
 
-  useEffect(() => {
+  const walletSync = useCallback(async () => {
+    socket.emit("xlm-fetch-wallet", userToken);
     socket.on("wallet-updated", args => {
       dispatch(balanceAction.setBalance(args));
     });
-  }, [dispatch, socket]);
+  }, [dispatch, socket, userToken]);
+
+  useEffect(() => {
+    walletSync();
+  }, [dispatch, walletSync]);
 
   return (
     <div className="App">
