deec2e35125386763024c57a4c776f8c3c0d5330
Fahad Hossain <fahad@fahadlabs.com>
2024-01-16 23:06:54 +0600

    Optimize socket transport to WebSocket
    Ensure socket connection uses WebSocket protocol exclusively for
    reliable communication and potential resolution of connection issues
    with long polling fallbacks. This change restricts transport to
    WebSocket, potentially increasing performance in environments where
    other transport mechanisms are less efficient or problematic.

--- src/context/soket.js
+++ src/context/soket.js
@@ -2,6 +2,6 @@ import React from "react";
 import { io } from "socket.io-client";
 import { LOCAL_SOCKET_SERVER } from "../config";
 
-export const socket = io(LOCAL_SOCKET_SERVER);
+export const socket = io(LOCAL_SOCKET_SERVER, { transports: ["websocket"] });
 // console.log("socket", socket);
 export const SocketContext = React.createContext();
