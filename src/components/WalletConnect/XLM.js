07946441a8917b33b19e50a042437b04a0cb29e1
gurpreet991 <gurpreet.singh@ellocentlabs.com>
2024-02-16 15:26:46 +0530

    feat(connect-modal): UI fixes

--- src/components/WalletConnect/XLM.js
+++ src/components/WalletConnect/XLM.js
@@ -5,6 +5,7 @@ import { useDispatch } from "react-redux";
 import { useSocket } from "../../context/socket";
 import * as balanceAction from "../../redux/xummBalance/action";
 import { toast } from "react-toastify";
+import { CrossIcon } from "../../Icons";
 
 const StellarWalletConnect = ({ setOpen }) => {
   const [uri, setUri] = useState(null);
@@ -41,18 +42,31 @@ const StellarWalletConnect = ({ setOpen }) => {
     socket.on("connection");
   };
 
+  const openApp = async () => {
+  };
+  const close = async () => {
+    setOpen(false)
+  };
   useEffect(() => {
     connectXlmWallet();
   }, []);
 
   return (
     <div className="p-8 bg-white">
-      <div className="desc">Scan QR code to connect</div>
+       <div onClick={close} className="absolute right-2 top-2">
+        <CrossIcon />
+      </div>
+      <span className="font-bold mb-2 text-lg">Connect using LOBSTR</span>  
+
+      <div className="desc mt-2 mb-1">Scan QR code to connect</div>
       {uri ? (
-        <QRCode size={256} style={{ height: "auto", maxWidth: "100%", width: "100%" }} value={uri} viewBox={`0 0 256 256`} />
+        <QRCode size={240} style={{ height: "auto", maxWidth: "100%", width: "100%" }} value={uri} viewBox={`0 0 256 256`} />
       ) : (
         "Loading..."
       )}
+      <button onClick={openApp} class="mt-4 bg-green w-full text-gray-800 p-2 font-semibold  border border-green-400 rounded">
+        Open LOBSTR App{" "}
+      </button>
     </div>
   );
 };
